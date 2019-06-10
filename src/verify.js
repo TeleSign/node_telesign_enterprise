052b3fa43c01b48f5c069630d337619ffcd2de65
Priyesh Jain <pjain@telesign.com>
2019-06-10 14:11:10 -0700

    EOL PushVerify

--- src/verify.js
+++ src/verify.js
@@ -3,7 +3,7 @@ const util = require('util')
 
 /***
  * The Verify API delivers phone-based verification and two-factor authentication using a time-based, one-time passcode
- * sent via SMS message, Voice call or Push Notification.
+ * sent via SMS message or Voice call.
  */
 class Verify {
 
@@ -17,7 +17,6 @@ class Verify {
         this.smsResource = "/v1/verify/sms"
         this.voiceResource = "/v1/verify/call"
         this.smartResource = "/v1/verify/smart"
-        this.pushResource = "/v2/verify/push"
         this.statusResource = "/v1/verify/%s"
         this.completionResource = "/v1/verify/completion/%s"
     }
@@ -89,31 +88,6 @@ class Verify {
         }
 
         this.rest.execute(callback, "POST", this.smartResource, params);
-    }
-
-    /***
-     * The Push Verify web service allows you to provide on-device transaction authorization for your end users. It
-     * works by delivering authorization requests to your end users via push notification, and then by receiving their
-     * permission responses via their mobile device's wireless Internet connection.
-     *
-     * See https://developer.telesign.com/docs/rest_api-verify-push for detailed API documentation.
-     *
-     * @param callback: Callback method to handle response.
-     * @param phoneNumber: Phone number to send the push notification.
-     * @param ucid: A string that specifies one of the use case codes.
-     * @param optionalParams: Dictionary of all optional parameters.
-     * transaction.
-     */
-    push(callback, phoneNumber, ucid, optionalParams=null) {
-        var params = {
-            phone_number: phoneNumber,
-            ucid: ucid
-        };
-        if (optionalParams !== null) {
-            params = Object.assign(params, optionalParams)
-        }
-
-        this.rest.execute(callback, "POST", this.pushResource, params);
     }
 
     /***
